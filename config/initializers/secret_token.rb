38ccba4c74116b6ded3fadc270374251f8427a55
curtisgiddings <curtisgiddings@gmail.com>
2013-04-02 22:03:00 -0400

    Initial rails boilerplate commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airgate::Application.config.secret_token = 'cc76aac17347a00068931787037aac768b5eae8fa6a145842732641d85d7dcacd7683731e97d7a83267f8559f5ac9a9aa4f8b7192276bc20694f532d4ed92336'
